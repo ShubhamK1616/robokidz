6ea6ad5489fd2ef16e33c7cc94990b6eca9c74fb
Shubham Kumar <shubham.shubpy@gmail.com>
2023-02-06 00:21:37 +0530

    Sidenav update

--- robokidz/src/Components/header.jsx
+++ robokidz/src/Components/header.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import DashboardIcon from 'remixicon-react/Dashboard3LineIcon'
 
 function Header() {
+  
+const b2cArray = [
+  {label : "Dashboard",
+  icon : <DashboardIcon color='white'/>
+},
+{
+  label : "Add Kit",
+  icon : <DashboardIcon color='white'/>
+}
+]
+const b2bArray = [
+  {label : "Dashboard",
+  icon : <DashboardIcon color='white'/>
+}
+]
+const adminArray = [
+  {label : "Label Printing",
+  icon : <DashboardIcon color='white'/>
+}
+]
+
+
   return (
     // <div className='w-screen h-screen grid grid-cols-12 '>    
     //     <div className='col-span-2 bg-[#4e73df]'>1</div>
@@ -13,7 +37,24 @@ function Header() {
         <div className='bg-[#4e73df]' style={{width : "15%", height : "100%"}}>
           <img style={{height : "15%", width : "100%"}} src='https://intranet.cyret.com/LMSDemo/img/small_logo.png' alt='logo' />
           <hr className='sidebar-divider' />
-          <div className='sidebar-heading'>B2C</div>
+          <div className='sidebar-heading'>B2C</div>  
+          <div className='flex flex-col'>
+            {b2cArray.map((item,index)=>(
+              <div className='flex flex-row px-4 py-1 text-white font-bold gap-x-4'>{item.icon}{item.label}</div>
+            ))}
+            </div>
+            <div className='sidebar-heading'>B2B</div>  
+          <div className='flex flex-col'>
+            {b2bArray.map((item,index)=>(
+              <div className='flex flex-row px-4 py-1 text-white font-bold gap-x-4'>{item.icon}{item.label}</div>
+            ))}
+            </div>
+            <div className='sidebar-heading'>ADMIN</div>  
+          <div className='flex flex-col'>
+            {adminArray.map((item,index)=>(
+              <div className='flex flex-row px-4 py-1 text-white font-bold gap-x-4'>{item.icon}{item.label}</div>
+            ))}
+            </div>
           </div>
         <div className='flex flex-col' style={{width : "85%"}}>
             <div className='' style={{height : "10%"}}></div>
