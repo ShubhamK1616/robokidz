23c0553383eba47bbbc64a28f40d8bead0c7ceef
Shubham Kumar <shubham.shubpy@gmail.com>
2023-02-13 23:50:42 +0530

    Sidenav Mobile responsive

--- robokidz/src/Components/Accordion.jsx
+++ robokidz/src/Components/Accordion.jsx
@@ -6,15 +6,15 @@ const Accordion = ({ title, content }) => {
   return (
     <div className="accordion-item gap-y-4 bg-[#f8f9fc]">
       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <div>{title}</div>
-        <div>{isActive ? '-' : '+'}</div>
+        <div className='flex text-[#4e73df] font-bold'>{title}</div>
+        <div className='text-[#4e73df] font-bold'>{isActive ? '-' : '+'}</div>
       </div>
       {isActive && 
       // <div className="accordion-content">{content}</div>
-      <div className="flex flex-row py-9">
-      <div className="flex mx-12 border rounded-xl bg-[#fff] shadow-md w-1/3 h-72">1</div>
-      <div className="flex mx-12 border rounded-xl bg-[#fff] shadow-md w-1/3 h-72">2</div>
-      <div className="flex mx-12 border rounded-xl bg-[#fff] shadow-md w-1/3 h-72">3</div>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-9 gap-x-6 px-6">
+      <div className="w-full flex border rounded-xl bg-[#fff] shadow-md h-72 justify-center items-center text-[#4e73df] font-bold">1</div>
+      <div className="w-full flex border rounded-xl bg-[#fff] shadow-md h-72 justify-center items-center text-[#4e73df] font-bold">2</div>
+      <div className="w-full flex border rounded-xl bg-[#fff] shadow-md h-72 justify-center items-center text-[#4e73df] font-bold">3</div>
 
     </div>
       }
--- robokidz/src/Components/sidenav.jsx
+++ robokidz/src/Components/sidenav.jsx
@@ -3,8 +3,12 @@ import DashboardIcon from 'remixicon-react/Dashboard3LineIcon';
 import LabelIcon from 'remixicon-react/PrinterLineIcon';
 import AddKitIcon from 'remixicon-react/LineChartLineIcon';
 import {Link} from 'react-router-dom';
+import HambIcon from 'remixicon-react/MenuLineIcon';
+import CloseIcon from 'remixicon-react/CloseLineIcon';
 
 function Sidenav () {
+
+  const [sideMenuOpenCloseState,setSideMenuOpenCloseState] = React.useState(false)
     const b2cArray = [
         {label : "Dashboard",
         icon : <DashboardIcon color='white'/>,
@@ -29,8 +33,11 @@ function Sidenav () {
       }
       ]
 
-return(
-        <div className='bg-[#4e73df]' style={{width : "12%", height : "100%"}}>
+return(<>
+         <div className={`md:hidden`}>
+          <button><HambIcon onClick={()=>setSideMenuOpenCloseState(true)}/></button>
+         </div>
+        <div className='bg-[#4e73df] hidden md:flex md:flex-col' style={{width : "12%", height : "100%"}}>
           <img style={{height : "10%", width : "100%", objectFit : "cover"}} src='https://intranet.cyret.com/LMSDemo/img/small_logo.png' alt='logo' />
           <hr className='sidebar-divider' />
           <div className='sidebar-heading'>B2C</div>  
@@ -54,6 +61,37 @@ return(
             ))}
             </div>
           </div>
+          {
+            sideMenuOpenCloseState && <div className='bg-blue-200 absolute z-[20]' style={{width : "50%", height : "100%"}}>
+              <div className='flex flex-row w-full'>
+              <img className='flex w-4/5' style={{ height : "130px", objectFit : "cover"}} src='https://intranet.cyret.com/LMSDemo/img/small_logo.png' alt='logo' />
+              <div className='flex w-1/5'><CloseIcon onClick={()=>setSideMenuOpenCloseState(false)}/></div>
+              </div>
+              
+          <hr className='sidebar-divider' />
+          <div className='sidebar-heading'>B2C</div>  
+          <div className='flex flex-col'>
+            {b2cArray.map((item,index)=>(
+              <Link className='flex flex-row p-4 text-white font-bold gap-x-4'to={item.link}>{item.icon}{item.label}</Link>
+            ))}
+            </div>
+            <hr className='sidebar-divider' />
+            <div className='sidebar-heading'>B2B</div>  
+          <div className='flex flex-col'>
+            {b2bArray.map((item,index)=>(
+              <Link className='flex flex-row p-4 text-white font-bold gap-x-4' to={item.link}>{item.icon}{item.label}</Link>
+            ))}
+            </div>
+            <hr className='sidebar-divider' />
+            <div className='sidebar-heading'>ADMIN</div>  
+          <div className='flex flex-col'>
+            {adminArray.map((item,index)=>(
+              <Link className='flex flex-row p-4 text-white font-bold gap-x-4' to={item.link}>{item.icon}{item.label}</Link>
+            ))}
+            </div>
+            </div>
+          }
+          </>
 )
 }
 
