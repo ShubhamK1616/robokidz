cf6399186f80b9e4306610fd4b4b08d6a2e25c96
Shubham Kumar <shubham.shubpy@gmail.com>
2023-02-13 11:47:47 +0530

    Table .jsx updated R

--- robokidz/src/Components/Table.jsx
+++ robokidz/src/Components/Table.jsx
@@ -19,21 +19,53 @@ function Table (props) {
         value : "hundred"
       }
       ]
+      const dataArray = [
+        {sr : "1",
+        date : "ten",
+        category : "Heavy Vehicle",
+        class : "Heavy",
+        quantity : "1",
+        printby : "printer"
+      },
+      {
+        sr : "2",
+        date : "ten",
+        category : "Heavy Vehicle",
+        class : "Heavy",
+        quantity : "1",
+        printby : "printer"
+      },
+      {sr : "3",
+      date : "ten",
+      category : "Heavy Vehicle",
+      class : "Heavy",
+      quantity : "1",
+      printby : "printer"
+      },
+      {
+        sr : "4",
+        date : "ten",
+        category : "Heavy Vehicle",
+        class : "Heavy",
+        quantity : "1",
+        printby : "printer"
+      }
+      ]
 return(
-    <div className='container border rounded shadow-[#3a3b451a] z-10'>
+    <div className='container border rounded shadow'>
     <div className='flex flex-col'>
       <div className='flex flex-row border py-3'><h1 className='text-[#4e73df] px-3 text-base font-bold'>Data Tables Example</h1></div>
       <div className='flex flex-row py-5 px-3'>
-        <div className='flex' style={{width : "75%"}}>Show<Pdrop options={printArray}/> entries</div>
+        <div className='flex' style={{width : "75%"}}><p className=' px-2'>Show</p>  <Pdrop className='' options={printArray}/> <p className=' px-2'>entries</p> </div>
         <div className='' style={{width : "25%"}}>
         <label>Search: </label>
-        <input className='border rounded' type="search" placeholder="" aria-label="Search"></input></div>
+        <input className='border-2 rounded focus:border-blue-300 outline-none focus:shadow-md' type="search" placeholder="" aria-label="Search"></input></div>
         
            
 
         </div>
       <div className='flex flex-row px-3'>
-      <table class="table align-middle mb-0 bg-white w-full border">
+      <table class="table align-middle mb-0 bg-white w-full border text-[#00000080]">
     <thead class="bg-light">
     <tr>
      <th className='border py-3'>Sr.No</th>
@@ -45,17 +77,37 @@ return(
     </tr>
  </thead>
 <tbody>
-{props?.tableData?.map(data => (<tr><td><img
-        src={data.thumbnail}
-        alt=""
-        style={{width: `45px`, height: `45px`}}
-        class="rounded-circle"
-        /></td><td>{data?.brand}</td><td>{data?.title}</td><td>{data?.stock}</td><td>{data?.category}</td><td>{data.rating}</td>
+{/* {props?.tableData?.map(data => (<tr><td>{data?.sr}</td><td>{data?.date}</td><td>{data?.category}</td><td>{data?.quantity}</td><td>{data?.printby}</td>
  <td>
-</td></tr>))}
+</td></tr>))} */}
+    {dataArray.map((item,index)=>( (<tr ><td className=' text-center'>{item?.sr}</td><td className=' text-center'>{item?.date}</td><td className=' text-center'>{item?.category}</td><td className=' text-center'>{item?.class}</td><td className=' text-center'>{item?.quantity}</td><td className=' text-center'>{item?.printby}</td>
+              <td>
+             </td></tr>)
+            ))}
+    
 </tbody>
+<thead class="bg-light">
+    <tr>
+     <th className='border py-3'>Sr.No</th>
+     <th className='border py-3'>Date</th>
+     <th className='border py-3'>Category</th>
+     <th className='border py-3'>Class</th>
+     <th className='border py-3'>Quantity</th>
+     <th className='border py-3'>Print By</th>
+    </tr>
+ </thead>
 </table>
+
       </div>
+      <div className='flex flex-row p-4'>
+        <div style={{width : "80%"}}>Showing 0 to 0 of 0 entries</div>
+        <div className='flex flex-wrap' style={{width : "20%"}}>
+        <a className=' px-4 py-3 border' href="#">Previous</a>
+        <a className=' px-4 py-3 border' href="#">1</a>
+        <a className=' px-4 py-3 border' href="#">2</a>
+        <a className=' px-4 py-3 border' href="#">Next</a>
+</div>
+     </div>
     </div>
     </div>
 )
--- robokidz/src/Components/dropdown.jsx
+++ robokidz/src/Components/dropdown.jsx
@@ -7,6 +7,7 @@ import ProfileIcon from 'remixicon-react/User3FillIcon';
 import SettingsIcon from 'remixicon-react/Settings4LineIcon';
 import LogtIcon from 'remixicon-react/LogoutBoxRLineIcon';
 import ActivityIcon from 'remixicon-react/ListCheckIcon';
+import logo from '../assets/images/th.png'
 
 
 
@@ -27,7 +28,9 @@ export default function Dropdown() {
 
   return (
   <div  className='relative inline-block text-left'>
-    <h3 onClick={clickHandler} className="flex flex-row cursor-pointer justify-center items-center rounded-full w-16 h-12 md:w-10 md:h-10 lg:w-16 lg:h-12">Sagar S</h3>
+    <h3 onClick={clickHandler} className="flex flex-row cursor-pointer justify-center items-center rounded-full w-full">Sagar S    <img src={logo} alt="logo" style={{width : "40px", height : "30px"}}
+     /></h3>
+  
 { showProfileMenu &&   <div className='origin-top-right absolute right-0 mt-1 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
     <div className='p-2 text-[#5a5c69]'>
     <div className='flex py-1 px-2 gap-x-4'><ProfileIcon />Profile</div>
